56b1d14a8d7f511579220f1aabd7cff886e44072
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2024-04-29 22:26:40 -0300

    acessando a planilha
    acessando a planilha com a funcoes da biblioteca openpyxl

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from PIL import Image, ImageDraw, ImageFont
 
 #abrindo planilha
 planilha = openpyxl.load_workbook('planilha_curso.xlsx')
+
+#acessando a pagina da planilha
+pagina_planilha = planilha['pagina1']
+
+#acessando as celulas da planilha
+for linha in pagina_planilha.iter_rows(min_row=2) :
+    curso = linha[0].value
+    print(curso)
+    break
