4834321b6605e18e829dae436bf46cc3a733a509
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2024-04-29 23:59:03 -0300

    finalizado
    projeto concluido

--- app.py
+++ app.py
@@ -21,9 +21,16 @@ for linha in pagina_planilha.iter_rows(min_row=2) :
     imagem_objeto = ImageDraw.Draw(imagem)
 
     #definindo a fonte do texto
-    fonte_nome = ImageFont.truetype('/fontes/OpenSans-Bold.ttf',50)
-    fonte_curso = ImageFont.truetype('/fontes/OpenSans-Bold.ttf',80)
-    fonte_geral = ImageFont.truetype('/fontes/OpenSans-Bold.ttf',30)
+    fonte_nome = ImageFont.truetype('./fontes/OpenSans-Bold.ttf',40)
+    fonte_curso = ImageFont.truetype('./fontes/OpenSans-Bold.ttf',50)
+    fonte_geral = ImageFont.truetype('./fontes/OpenSans-Regular.ttf',28)
 
-    imagem.show()
-    break
+    #adicionando o texto na imagem
+    imagem_objeto.text((355,430), nome_aluno, 'black', font=fonte_nome)
+    imagem_objeto.text((405,499), data_conclusao, 'black', font=fonte_geral)
+    imagem_objeto.text((355,540), curso, 'black', font=fonte_curso)
+    imagem_objeto.text((650,670), carga_horaria, 'black', font=fonte_geral)
+
+    #salvando as imagem modificadas
+    imagem.save(f'./certificados_salvos/{nome_aluno}.png')
+    
